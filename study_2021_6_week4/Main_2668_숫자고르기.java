6ddf678e17d8367923a6c62fa32a50e6996e5f4b
annamon <annamon20@likelion.org>
2021-06-21 14:58:30 +0900

    2021.06.20 일

---
+++ study_2021_6_week4/Main_2668_숫자고르기.java
@@ -0,0 +1,23 @@
+package study_2021_6_week4;
+
+/**
+ * @author anna
+ * @date 2021.06.20
+ * @summary 
+ * BOJ-silver4  64MB/2s (12884KB/116ms)
+ * A,B,C,D,E,F 0or1개로 시작
+ * 그 다음 A, F, C 각각 n개
+ * 그 다음 A,B,C,D,E,F 0or1개 끝
+ * 규칙 지키는 경우 Infected! 아닌 경우 Good 출력
+ * 
+ * T : 테케 개수  (≤20)
+ * 알파벳 최대 200개
+ * chromosome : 규칙 염색체 (정규식)
+ */
+public class Main_2668_숫자고르기 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
