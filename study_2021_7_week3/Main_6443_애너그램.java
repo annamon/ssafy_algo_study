34234c851a3a800fa72d8b781211febcfbc4bcae
annamon <annamon20@likelion.org>
2021-07-26 21:15:26 +0900

    2021.07.25 일 스터디 공통문제

--- study_2021_7_week3/Main_6443_애너그램.java
+++ study_2021_7_week3/Main_6443_애너그램.java
@@ -13,10 +13,16 @@ import java.util.TreeSet;
  * @author anna
  * @date 2021.07.25
  * @summary 
- * BOJ-gold4  128MB/1s (KB/ms)
+ * BOJ-gold4  128MB/1s (14516KB/276ms)
  * 입력받은 영단어 철자들로 만들 수 있는 모든 단어 출력하기
  * 같은 단어가 여러 번 만들어지는 경우 한 번만 출력
  * 알파벳 순서로 출력
+ * 애너그램 수 10만개 이하, 단어 길이 20이하
+ * 
+ * N : 테스트케이스 수
+ * L : 단어 길이
+ * word[] : 단어 배열, 아스키코드 저장
+ * 조합 메모리초과, np로 풀기
  */
 public class Main_6443_애너그램 {
 
@@ -26,31 +32,41 @@ public class Main_6443_애너그램 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N = Integer.parseInt(br.readLine());
 		for (int i = 0; i < N; i++) {
 			s = br.readLine();
 			L = s.length();
 			word = new int[L];
-			for (int j = 0; j < L; j++) {
-				word[j] = s.charAt(j);
-			}
-			Arrays.sort(word, Collections.reverseOrder());
+			for (int j = 0; j < L; j++) word[j] = s.charAt(j);
+			Arrays.sort(word); //오름차순
+			//next Permutation 
 			do {
-				
+				for(int n : word) bw.write((char)n);
+				bw.newLine();
 			} while (np(L-1));
-			
 		}
 		bw.flush();
 	}
 	
+	//NP
 	private static boolean np(int size) {
 		int i = size;
 		while(i>0 && word[i-1]>=word[i]) i--;
 		if(i==0) return false;
 		int j = size;
-		while(p[i-1] >= word[j]) j--;
+		while(word[i-1] >= word[j]) j--;
 		int temp = word[i-1];
 		word[i-1] = word[j];
 		word[j] = temp;
+		int k = size;
+		while(i<k) {
+			temp = word[i];
+			word[i] = word[k];
+			word[k] = temp;
+			i++;
+			k--;
+		}
+		return true;
 	}
 	
 /*	조합으로 풀기 : 메모리 초과
